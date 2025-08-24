8a86365de6c844e8b21e6a9cdbad3a264260a05b
yoannfb <yoannfb@hotmail.com>
2025-08-24 21:49:04 +0200

    maj faq.css

--- public/faq.php
+++ public/faq.php
@@ -6,21 +6,20 @@ include('includes/header.php'); ?>
         <div class="faq">
             <h5 class="title">cliquer pour obtenir la réponse</h5>
             <div  id="lien" class="d-flex">
-                
                 <div class="text col-lg-4 col-md-4 d-flex align-items-center m-5 mx-1">Comment se déroule une séance d'hypnose spirituelle, régressive énergétique ?</div>
-                <p class="answer col-lg-6 col-md-6 col-sm-6 pt-4 ps-5 mx-auto">Une séance commence par un entretien pour comprendre vos besoins et vos objectifs. Ensuite, vous serez guidé dans un état de relaxation profond grâce à des techniques d’hypnose. Une fois en état hypnotique, nous explorerons ensemble des vies passées ou des mémoires anciennes, tout en utilisant des techniques énergétiques pour libérer les blocages et favoriser la guérison.</p>
+                <div class="answer col-lg-6 col-md-6 col-sm-6 pt-4 ps-5 mx-auto">Une séance commence par un entretien pour comprendre vos besoins et vos objectifs. Ensuite, vous serez guidé dans un état de relaxation profond grâce à des techniques d’hypnose. Une fois en état hypnotique, nous explorerons ensemble des vies passées ou des mémoires anciennes, tout en utilisant des techniques énergétiques pour libérer les blocages et favoriser la guérison.</div>
             </div>
             <div  id="lien" class="d-flex">
-                <p class="answer col-lg-6 col-md-6 col-sm-6 pt-4 ps-5 mx-auto">La plupart des gens peuvent atteindre un état d’hypnose. Il est important de se sentir en confiance et à l’aise avec le praticien. L’état d’hypnose est simplement un état de relaxation profonde où l’esprit est plus ouvert aux suggestions. J’aime à dire que tous ceux qui savent rêver peuvent être hypnotisés !</p>
+                <div class="answer col-lg-6 col-md-6 col-sm-6 pt-4 ps-5 mx-auto">La plupart des gens peuvent atteindre un état d’hypnose. Il est important de se sentir en confiance et à l’aise avec le praticien. L’état d’hypnose est simplement un état de relaxation profonde où l’esprit est plus ouvert aux suggestions. J’aime à dire que tous ceux qui savent rêver peuvent être hypnotisés !</div>
                 <div class="text col-lg-4 col-md-4 d-flex align-items-center m-5 mx-1">Est-ce que tout le monde peut être hypnotisé ?</div>
             </div>
             <div  id="lien" class="d-flex">
                 <div class="text col-lg-4 col-md-4 d-flex align-items-center m-5 mx-1">Vais-je me souvenir de ce qui se passe pendant la séance?</div>
-                <p class="answer col-lg-6 col-md-6 col-sm-6 pt-4 ps-5 mx-auto">Oui, la plupart des personnes se souviennent de ce qui se passe pendant une séance d’hypnose spirituelle, régressive, énergétique. Vous serez conscient et en contrôle durant toute l’expérience. Certaines personnes peuvent même avoir des souvenirs plus vifs et des prises de conscience après la séance. J’enregistre toute la séance et vous fais parvenir l’enregistrement, ce qui vous permet de vous rappeler et d’avoir une sorte de deuxième séance.</p>
+                <div class="answer col-lg-6 col-md-6 col-sm-6 pt-4 ps-5 mx-auto">Oui, la plupart des personnes se souviennent de ce qui se passe pendant une séance d’hypnose spirituelle, régressive, énergétique. Vous serez conscient et en contrôle durant toute l’expérience. Certaines personnes peuvent même avoir des souvenirs plus vifs et des prises de conscience après la séance. J’enregistre toute la séance et vous fais parvenir l’enregistrement, ce qui vous permet de vous rappeler et d’avoir une sorte de deuxième séance.</div>
             </div>
             <div  id="lien" class="d-flex">
-                <p class="answer col-lg-6 col-md-6 col-sm-6 pt-4 ps-5 mx-auto">Oui, l’Hypnose SRE est une pratique sûre lorsqu’elle est réalisée par un praticien qualifié et expérimenté. Elle respecte toujours le rythme et les limites de la personne. Vous ne serez jamais poussé à explorer quelque chose que vous n’êtes pas prêt à affronter
-                    <u>Cependant, l'hypnose spirituelle, régressive énergétique ne remplace pas un avis médical.</u></p>
+                <div class="answer col-lg-6 col-md-6 col-sm-6 pt-4 ps-5 mx-auto">Oui, l’Hypnose SRE est une pratique sûre lorsqu’elle est réalisée par un praticien qualifié et expérimenté. Elle respecte toujours le rythme et les limites de la personne. Vous ne serez jamais poussé à explorer quelque chose que vous n’êtes pas prêt à affronter
+                    <br><span class="souligner">Cependant, l'hypnose spirituelle, régressive énergétique ne remplace pas un avis médical.</span></div>
                 <div class="text col-lg-4 col-md-4 d-flex align-items-center m-5 mx-1">L’hypnose spirituelle, régressive énergétique est-elle sûre ?</div>
             </div>
             <div  id="lien" class="d-flex">
@@ -35,20 +34,20 @@ include('includes/header.php'); ?>
                 </div>
             </div>
             <div  id="lien" class="d-flex">
-                <p class="answer col-lg-6 col-md-6 col-sm-6 pt-4 ps-5 mx-auto">Le nombre de séances varie en fonction des besoins individuels. Certaines personnes peuvent ressentir des bienfaits dès la première séance, tandis que d’autres peuvent nécessiter plusieurs sessions pour atteindre leurs objectifs. </p>
+                <div class="answer col-lg-6 col-md-6 col-sm-6 pt-4 ps-5 mx-auto">Le nombre de séances varie en fonction des besoins individuels. Certaines personnes peuvent ressentir des bienfaits dès la première séance, tandis que d’autres peuvent nécessiter plusieurs sessions pour atteindre leurs objectifs. </div>
                 <div class="text col-lg-4 col-md-4 d-flex align-items-center m-5 mx-1">Combien de séances sont nécessaires?</div>
             </div>
             <div  id="lien" class="d-flex">
                 <div class="text col-lg-4 col-md-4 d-flex align-items-center m-5 mx-1">Que dois-je faire pour me préparer à une séance?</div>
-                <p class="answer col-lg-6 col-md-6 col-sm-6 pt-4 ps-5 mx-auto">Il est conseillé de venir à la séance avec une intention claire ou une question que vous souhaitez explorer. Portez des vêtements confortables et assurez-vous d’être bien hydraté. Il est également utile de ne boire ni café, ni thé dans les deux jours précédant la séance afin de pouvoir être parfaitement détendu.</p>
+                <div class="answer col-lg-6 col-md-6 col-sm-6 pt-4 ps-5 mx-auto">Il est conseillé de venir à la séance avec une intention claire ou une question que vous souhaitez explorer. Portez des vêtements confortables et assurez-vous d’être bien hydraté. Il est également utile de ne boire ni café, ni thé dans les deux jours précédant la séance afin de pouvoir être parfaitement détendu.</div>
             </div>
             <div  id="lien" class="d-flex">
-                <p class="answer col-lg-6 col-md-6 col-sm-6 pt-4 ps-5 mx-auto">Non, vous ne pouvez pas être influencé ou manipulé contre votre volonté pendant l’hypnose. Vous restez en contrôle de vos pensées et de vos actions tout au long de la séance. L’hypnose spirituelle, régressive, énergétique est un processus collaboratif où vous êtes un participant actif.</p>
+                <div class="answer col-lg-6 col-md-6 col-sm-6 pt-4 ps-5 mx-auto">Non, vous ne pouvez pas être influencé ou manipulé contre votre volonté pendant l’hypnose. Vous restez en contrôle de vos pensées et de vos actions tout au long de la séance. L’hypnose spirituelle, régressive, énergétique est un processus collaboratif où vous êtes un participant actif.</div>
                 <div class="text col-lg-4 col-md-4 d-flex align-items-center m-5 mx-1">Est-ce que je peux être influencé ou manipulé pendant l'hypnose ?</div>
             </div>
             <div  id="lien" class="d-flex">
                 <div class="text col-lg-4 col-md-4 d-flex align-items-center m-5 mx-1">Quels sont les bénéfices de l'hypnose spirituelle, régressive, énergétique ?</div>
-                <p class="answer col-lg-6 col-md-6 col-sm-6 pt-4 ps-5 mx-auto">Les bénéfices peuvent inclure une meilleure compréhension de soi, la libération de vieux traumatismes, une réduction du stress et de l'anxiété, une amélioration de la clarté émotionnelle et mentale, et un sentiment général de paix et d'harmonie.</p>
+                <div class="answer col-lg-6 col-md-6 col-sm-6 pt-4 ps-5 mx-auto">Les bénéfices peuvent inclure une meilleure compréhension de soi, la libération de vieux traumatismes, une réduction du stress et de l'anxiété, une amélioration de la clarté émotionnelle et mentale, et un sentiment général de paix et d'harmonie.</div>
             </div>
             <div  id="lien" class="d-flex">
                 <div class="answer col-lg-6 col-md-6 col-sm-6 pt-4 ps-5 mx-auto">
@@ -64,32 +63,32 @@ include('includes/header.php'); ?>
             </div>
             <div  id="lien" class="d-flex">
                 <div class="text col-lg-4 col-md-4 d-flex align-items-center m-5 mx-1">Est-ce que les soins énergétiques sont sûrs ?</div>
-                <p class="answer col-lg-6 col-md-6 col-sm-6 pt-4 ps-5 mx-auto">Oui, les soins énergétiques sont généralement considérés comme sûrs lorsqu’ils sont pratiqués par un thérapeute qualifié. Ils sont non invasifs et complètent souvent d’autres formes de traitement médical ou thérapeutique. 
-                    <u>Cependant, ils ne remplacent pas un avis médical.</u></p>
+                <div class="answer col-lg-6 col-md-6 col-sm-6 pt-4 ps-5 mx-auto">Oui, les soins énergétiques sont généralement considérés comme sûrs lorsqu’ils sont pratiqués par un thérapeute qualifié. Ils sont non invasifs et complètent souvent d’autres formes de traitement médical ou thérapeutique. 
+                    <br><span class="souligner">Cependant, ils ne remplacent pas un avis médical.</span></div>
             </div>
             <div  id="lien" class="d-flex">
-                <p class="answer col-lg-6 col-md-6 col-sm-6 pt-4 ps-5 mx-auto">Non, il n’est pas nécessaire de croire en l’énergie pour bénéficier des soins énergétiques. De nombreuses personnes ressentent les bienfaits même si elles sont sceptiques au départ. Ce qui est important, c’est d’être ouvert et réceptif à l’expérience et surtout, que la démarche vienne de nous.</p>
+                <div class="answer col-lg-6 col-md-6 col-sm-6 pt-4 ps-5 mx-auto">Non, il n’est pas nécessaire de croire en l’énergie pour bénéficier des soins énergétiques. De nombreuses personnes ressentent les bienfaits même si elles sont sceptiques au départ. Ce qui est important, c’est d’être ouvert et réceptif à l’expérience et surtout, que la démarche vienne de nous.</div>
                 <div class="text col-lg-4 col-md-4 d-flex align-items-center m-5 mx-1">Est-ce que je dois croire en l'énergie pour que les soins fonctionnent ?</div>
             </div>
             <div  id="lien" class="d-flex">
                 <div class="text col-lg-4 col-md-4 d-flex align-items-center m-5 mx-1">Combien de séances sont nécessaires ?</div>
-                <p class="answer col-lg-6 col-md-6 col-sm-6 pt-4 ps-5 mx-auto">Le nombre de séances nécessaires varie en fonction de vos besoins individuels et de vos objectifs. Certaines personnes ressentent des bienfaits après une seule séance, tandis que d'autres peuvent nécessiter plusieurs sessions pour des résultats durables. Nous discuterons ensemble de ce qui est le mieux pour vous.</p>
+                <div class="answer col-lg-6 col-md-6 col-sm-6 pt-4 ps-5 mx-auto">Le nombre de séances nécessaires varie en fonction de vos besoins individuels et de vos objectifs. Certaines personnes ressentent des bienfaits après une seule séance, tandis que d'autres peuvent nécessiter plusieurs sessions pour des résultats durables. Nous discuterons ensemble de ce qui est le mieux pour vous.</div>
             </div>
             <div  id="lien" class="d-flex">
-                <p class="answer col-lg-6 col-md-6 col-sm-6 pt-4 ps-5 mx-auto">Il est conseillé de porter des vêtements confortables et d'être bien hydraté. Il est également utile de prendre un moment de détente avant la séance.</p>
+                <div class="answer col-lg-6 col-md-6 col-sm-6 pt-4 ps-5 mx-auto">Il est conseillé de porter des vêtements confortables et d'être bien hydraté. Il est également utile de prendre un moment de détente avant la séance.</div>
                 <div class="text col-lg-4 col-md-4 d-flex align-items-center m-5 mx-1">Que dois-je faire pour me préparer à une séance ?</div>
             </div>
             <div  id="lien" class="d-flex">
                 <div class="text col-lg-4 col-md-4 d-flex align-items-center m-5 mx-1">Est-ce que je peux continuer mes traitements médicaux en parallèle ?</div>
-                <p class="answer col-lg-6 col-md-6 col-sm-6 pt-4 ps-5 mx-auto">Oui, les soins énergétiques peuvent-être utilisés en complément de vos traitements médicaux. Il est primordial de continuer à suivre les recommandations de votre médecin. <u>Les soins énergétiques ne se substituent pas à un avis médical.</u></p>
+                <div class="answer col-lg-6 col-md-6 col-sm-6 pt-4 ps-5 mx-auto">Oui, les soins énergétiques peuvent-être utilisés en complément de vos traitements médicaux. Il est primordial de continuer à suivre les recommandations de votre médecin.<br><span class="souligner">Les soins énergétiques ne se substituent pas à un avis médical.</span></div>
             </div>
             <div  id="lien" class="d-flex">
-                <p class="answer col-lg-6 col-md-6 col-sm-6 pt-4 ps-5 mx-auto"> Pendant une séance, vous pouvez ressentir des sensations de chaleur, de picotements, ou un profond sentiment de relaxation. Certaines personnes peuvent également ressentir des émotions qui émergent. Après une séance, vous pouvez vous sentir revitalisé, apaisé ou émotionnellement équilibré. Les réactions peuvent varier d'une personnes à l'autre.</p>
+                <div class="answer col-lg-6 col-md-6 col-sm-6 pt-4 ps-5 mx-auto"> Pendant une séance, vous pouvez ressentir des sensations de chaleur, de picotements, ou un profond sentiment de relaxation. Certaines personnes peuvent également ressentir des émotions qui émergent. Après une séance, vous pouvez vous sentir revitalisé, apaisé ou émotionnellement équilibré. Les réactions peuvent varier d'une personnes à l'autre.</div>
                 <div class="text col-lg-4 col-md-4 d-flex align-items-center m-5 mx-1">Que vais-je ressentir pendant et après une séance ?</div>
             </div>
             <div  id="lien" class="d-flex">
                 <div class="text col-lg-4 col-md-4 d-flex align-items-center m-5 mx-1">Est-ce que les soins énergétiques peuvent aider avec des problèmes spécifiques?</div>
-                <p class="answer col-lg-6 col-md-6 col-sm-6 pt-4 ps-5 mx-auto">Les soins énergétiques peuvent-être bénéfiques pour une variété de problèmes, y compris le stress, l'anxiété, les douleurs chroniques, les troubles du sommeil et bien d'autres. <u>Cependant, ils ne remplacent pas un diagnostic ou un traitement médical. Il est toujours recommandé de consulter un médecin pour des problèmes de santé spécifiques.</u></p>
+                <div class="answer col-lg-6 col-md-6 col-sm-6 pt-4 ps-5 mx-auto">Les soins énergétiques peuvent-être bénéfiques pour une variété de problèmes, y compris le stress, l'anxiété, les douleurs chroniques, les troubles du sommeil et bien d'autres.<br><span class="souligner">Cependant, ils ne remplacent pas un diagnostic ou un traitement médical. Il est toujours recommandé de consulter un médecin pour des problèmes de santé spécifiques.</span></div>
             </div>
         </div>
 
